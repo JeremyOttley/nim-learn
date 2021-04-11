a89dab4ec722123e885252444673755149310320
Jeremy Ottley <Gazaunga@gmail.com>
2021-04-11 14:07:11 -0400

    Create htdp.nim

---
+++ htdp.nim
@@ -0,0 +1,7 @@
+# Design the function string-first, which extracts the first character from a non-empty string. Don’t worry about empty strings.
+
+# Design the function string-last, which extracts the last character from a non-empty string.
+
+# Design the function string-rest, which produces a string like the given one with the first character removed.
+
+# Design the function string-remove-last, which produces a string like the given one with the last character removed.
