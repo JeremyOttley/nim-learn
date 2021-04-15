3e8517268a4a8211f08b4dfffcc60ed9de22490b
Jeremy Ottley <Gazaunga@gmail.com>
2021-04-15 09:54:07 -0400

    Create result.nim

---
+++ result.nim
@@ -0,0 +1,9 @@
+proc keepOdds(a: seq[int]): seq[int] =
+  # result = @[]          
+  for number in a:
+    if number mod 2 == 1:
+      result.add(number)
+
+
+let f = @[1, 6, 4, 43, 57, 34, 98]
+echo keepOdds(f)
