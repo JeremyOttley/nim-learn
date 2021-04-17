f0f47d50f8ffb424f720e2d94d26bfe50aee015e
Jeremy Ottley <Gazaunga@gmail.com>
2021-04-17 13:50:16 -0400

    Create type.nim

---
+++ type.nim
@@ -0,0 +1,17 @@
+type
+  Computer = object
+    manufacturer: string
+    cpu: string
+    powerConsumption: float
+    ram: int # GB
+    ssd: int # GB
+    quantity: int
+    price: float
+
+var workLaptop = Computer(manufacturer: "Dell", 
+                          cpu: "Intel", 
+                          powerConsumption: 89.15, 
+                          ram: 32, 
+                          ssd: 250, 
+                          quantity: 1, 
+                          price: 1600.00)
