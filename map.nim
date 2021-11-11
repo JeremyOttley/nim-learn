04e1455560f4eec23847272bea2a824b408ae127
Jeremy Ottley <Gazaunga@gmail.com>
2021-11-11 18:27:59 -0500

    Update map.nim

--- map.nim
+++ map.nim
@@ -1,5 +1,22 @@
+## "map" datastructure
+
 import tables
 
 var myMap: Table[string, string] = {"a": "apple", "b": "banana"}.toTable
 #myMap.del("a")
 #myMap.add("a")
+
+
+## "map" function
+
+import sequtils
+
+let 
+  a = @[1, 2, 3, 4]
+  b = a.map(proc(x: int): int = x * 2)
+
+## "map" function with iterator (preferred in nim)
+
+let
+  a = @[1, 2, 3, 4]
+  b = a.mapIt(4 * it)
