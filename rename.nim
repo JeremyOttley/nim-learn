2de87e2275a195e659f3b9bab1fe807c5e0c8c65
Jeremy Ottley <Gazaunga@gmail.com>
2021-11-12 20:54:53 -0500

    Create rename.nim

---
+++ rename.nim
@@ -0,0 +1,19 @@
+# this script was used to rename a series of photos used in a project for the Manifold publishing platform
+
+import os, nre
+
+var images: seq[string]
+
+proc collectImages(): int =
+  for jpg in walkFiles("*.jpg"):
+    images.add(jpg)
+  for jpeg in walkFiles("*.jpeg"):
+    images.add(jpeg)
+  for png in walkFiles("*.png"):
+    images.add(png)
+  for bmp in walkFiles("*.bmp"):
+    images.add(bmp)
+  result = 1
+
+for image in images:
+  moveFiles(image, image.replace(re"\w+_\d+-", ""))
