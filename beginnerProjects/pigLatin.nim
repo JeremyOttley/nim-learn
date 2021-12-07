a3a8d695383eaab8eec2ebfec9063cd196cb79a5
Jeremy Ottley <Gazaunga@gmail.com>
2021-12-07 15:25:05 -0500

    Update pigLatin.nim

--- beginnerProjects/pigLatin.nim
+++ beginnerProjects/pigLatin.nim
@@ -1,1 +1,16 @@
+import strutils
+import sequtils
+import algorithm
+
+#proc reverseString(s: string): string =
+#  result = s.reversed.join
+
+proc addSuffix(s: string): string =
+  result = s & "ay"
+
+proc pLatinize(s: string): string {. discardable .} =
+  result = addSuffix(toSeq(s).rotatedLeft(1).join)
+
+when isMainModule:
+  echo pLatinize(readLine(stdin))
 
