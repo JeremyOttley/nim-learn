749d0698f230da95932140c8eb0207b4a86214d9
Jeremy Ottley <Gazaunga@gmail.com>
2021-12-14 10:06:08 -0500

    Update countVowels.nim

--- beginnerProjects/countVowels.nim
+++ beginnerProjects/countVowels.nim
@@ -1,1 +1,13 @@
+import nre
+from sequtils import toSeq # toSeq collides with nre/toSeq which is different
 
+let vowels = re"[aeoui]"
+
+let s = sequtils.toSeq("Jeremy".findIter(vowels))
+
+proc countVowels(s: string): int {. discardable .} =
+  result = sequtils.toSeq(s.findIter(vowels)).len
+
+
+when isMainModule:
+  echo "Jeremy has: " & $countVowels(readLine(stdin)) & " vowels"
