977ef1f405f18ddf8f94f02b62731f98cb88ef42
Jeremy Ottley <Gazaunga@gmail.com>
2021-12-14 10:19:12 -0500

    Update countVowels.nim

--- beginnerProjects/countVowels.nim
+++ beginnerProjects/countVowels.nim
@@ -3,8 +3,6 @@ from sequtils import toSeq # toSeq collides with nre/toSeq which is different
 
 let vowels = re"[aeoui]"
 
-let s = sequtils.toSeq("Jeremy".findIter(vowels))
-
 proc countVowels(s: string): int {. discardable .} =
   result = sequtils.toSeq(s.findIter(vowels)).len
 
