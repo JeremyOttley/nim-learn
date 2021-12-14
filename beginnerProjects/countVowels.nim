4e21c7260269fcd7b01d776e6906e1a9f701f255
Jeremy Ottley <Gazaunga@gmail.com>
2021-12-14 10:27:52 -0500

    Update countVowels.nim

--- beginnerProjects/countVowels.nim
+++ beginnerProjects/countVowels.nim
@@ -3,9 +3,14 @@ from sequtils import toSeq # toSeq collides with nre/toSeq which is different
 
 let vowels = re"[aeoui]"
 
+var userInput = readLine(stdin)
+
+proc hasVowels(s: string): seq[RegexMatch] =
+  result = sequtils.toSeq(s.findIter(vowels))
+
 proc countVowels(s: string): int {. discardable .} =
   result = sequtils.toSeq(s.findIter(vowels)).len
 
 
 when isMainModule:
-  echo "Jeremy has: " & $countVowels(readLine(stdin)) & " vowels"
+  echo userInput & " has " & $countVowels(userInput) & " vowels: " & hasVowels(userInput)[0].match & " and " & hasVowels(userInput)[1].match
