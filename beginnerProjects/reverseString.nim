efa0fd6dc8c56f877d80c860d8c4d6a0f26dd1e6
Jeremy Ottley <Gazaunga@gmail.com>
2021-12-14 10:05:27 -0500

    Update reverseString.nim

--- beginnerProjects/reverseString.nim
+++ beginnerProjects/reverseString.nim
@@ -1,8 +1,11 @@
-import strutils
-import algorithm
+import strutils # join()
+import sequtils # @ (to seq)
+import algorithm # reversed()
+
+var userInput = readLine(stdin)
 
 proc reverseString(s: string): string =
-  result = s.reversed.join
+  result = @s.reversed.join("")
 
 when isMainModule:
-  reverseString(readLine(stdin))
+  echo userInput  & " reversed is: " & reverseString(userInput)
