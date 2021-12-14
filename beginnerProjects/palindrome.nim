391fa2734dbfd11f9a556cb2fb64f6be7466e09d
Jeremy Ottley <Gazaunga@gmail.com>
2021-12-14 15:06:41 -0500

    Update palindrome.nim

--- beginnerProjects/palindrome.nim
+++ beginnerProjects/palindrome.nim
@@ -1,11 +1,12 @@
 import algorithm
 import strutils
+import sequtils
 
 var userInput = readLine(stdin)
 
 proc isPalindrome(s: string): bool =
-  result = s.toLower == s.toLower.reversed.join()
+  result = @s.filterIt(it != ' ').join == s.toLower.filterIt(it != ' ').reversed.join()
 
-
+  # @s.filterIt(it != ' ')
 when isMainModule:
   echo isPalindrome(userInput)
