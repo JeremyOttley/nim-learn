7d11728b207ef793dc96feb3da517d9a0c542ba8
Jeremy Ottley <Gazaunga@gmail.com>
2022-04-18 15:01:34 -0400

    Update htdp.nim

--- htdp.nim
+++ htdp.nim
@@ -1,6 +1,22 @@
 # Design the function string-first, which extracts the first character from a non-empty string. Don’t worry about empty strings.
 
+func stringFirst(s: string): char =
+  @s[0]
+
+var userInput = readLine(stdin)
+
+when isMainModule:
+  echo "The first char of " & userInput & " is: " & stringFirst(userInput)
+
 # Design the function string-last, which extracts the last character from a non-empty string.
+
+func stringLast(s: string): char =
+  @s[s.high]
+
+var userInput = readLine(stdin)
+
+when isMainModule:
+  echo "The last char of " & userInput & " is: " & stringLast(userInput)
 
 # Design the function string-rest, which produces a string like the given one with the first character removed.
 
