a300328d89e2821d9f18f450285c73cd37d4ca20
Jeremy Ottley <Gazaunga@gmail.com>
2022-04-19 13:36:59 -0400

    Update pigLatin.nim

--- beginnerProjects/pigLatin.nim
+++ beginnerProjects/pigLatin.nim
@@ -5,10 +5,10 @@ import algorithm
 var userInput = readLine(stdin)
 
 proc addSuffix(s: string): string =
-  result = s & "ay"
+  s & "ay"
 
 proc pLatinize(s: string): string {. discardable .} =
-  result = addSuffix(toSeq(s).rotatedLeft(1).join)
+  addSuffix(toSeq(s).rotatedLeft(1).join)
 
 when isMainModule:
   echo userInput & " in Pig Latin is: " & pLatinize(userInput)
