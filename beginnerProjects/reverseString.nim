3349e3776d746568314db552f47e03a9455f3520
Jeremy Ottley <Gazaunga@gmail.com>
2022-04-19 17:42:29 -0400

    Update reverseString.nim

--- beginnerProjects/reverseString.nim
+++ beginnerProjects/reverseString.nim
@@ -8,4 +8,4 @@ func reverseString(s: string): string =
   @s.reversed.join("")
 
 when isMainModule:
-  echo userInput  & " reversed is: " & reverseString(userInput)
+  stdout.writeLine(userInput  & " reversed is: " & reverseString(userInput))
