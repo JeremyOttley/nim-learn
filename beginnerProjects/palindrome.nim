9874009042b356f0cf7e822533d8ee2039a5666e
Jeremy Ottley <Gazaunga@gmail.com>
2022-04-19 13:36:11 -0400

    Update palindrome.nim

--- beginnerProjects/palindrome.nim
+++ beginnerProjects/palindrome.nim
@@ -4,7 +4,7 @@ import sequtils
 
 var userInput = readLine(stdin)
 
-proc isPalindrome(s: string): bool =
+func isPalindrome(s: string): bool =
   var origString = @s.filterIt(it != ' ').join
   var reversedString = s.toLower.filterIt(it != ' ').reversed.join()
   result = origString == reversedString
