682499e2cb2453158c743c40f2782e3d352c45f8
Jeremy Ottley <Gazaunga@gmail.com>
2022-04-22 15:35:55 -0400

    Update zodiac.nim

--- beginnerProjects/zodiac.nim
+++ beginnerProjects/zodiac.nim
@@ -1,1 +1,64 @@
+import strutils
+import xmltree
+import httpclient
+import q
 
+const
+  base: string = "https://www.astrology.com/horoscope/daily"
+  aries: string = "aries"
+  taurus: string = "taurus"
+  gemini: string = "gemini"
+  cancer: string = "cancer"
+  leo: string = "leo"
+  virgo: string = "virgo"
+  libra: string = "libra"
+  scorpio: string = "scorpio"
+  sagittarius: string = "sagittarius"
+  capricorn: string = "capricorn"
+  aquarius: string = "aquarius"
+  pisces: string = "pisces"
+
+
+
+proc query() =
+  stdout.writeLine("Hey baby, what's your sign?: ", "\n")
+
+proc getFortune(sign: string): string =
+  var client = newHttpClient()
+  var response = client.get(base & "/" & sign & ".html")
+  var doc = q(response.body)
+  var pagetext = doc.select("p")
+  echo "\n", capitalizeAscii(sign) & ": ", "\n", pagetext[1].innerText
+
+#echo pagetext[1].innerText
+
+when isMainModule:
+  query()
+  var sign = readLine(stdin)
+  case sign:
+  of "aries":
+    echo getFortune(aries)
+  of "taurus":
+    echo getFortune(taurus)
+  of "gemini":
+    echo getFortune(gemini)
+  of "cancer":
+    echo getFortune(cancer)
+  of "leo":
+    echo getFortune(leo)
+  of "virgo":
+    echo getFortune(virgo)
+  of "libra":
+    echo getFortune(libra)  
+  of "scorpio":
+    echo getFortune(scorpio)
+  of "sagittarius":
+    echo getFortune(sagittarius)
+  of "capricorn":
+    echo getFortune(capricorn)
+  of "aquarius":
+    echo getFortune(aquarius)
+  of "pisces":
+    echo getFortune(pisces)
+  else:
+    echo "Is that a real sign?! Try again."
