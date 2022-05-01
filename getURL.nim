f3b4456a30b6201624247d9af10e2f2061154334
Jeremy Ottley <Gazaunga@gmail.com>
2022-05-01 11:40:27 -0400

    Update getURL.nim

--- getURL.nim
+++ getURL.nim
@@ -1,5 +1,12 @@
 import os, httpclient
 
+proc retrieve(url: string): string =
+  var browser = newHttpClient()
+  var response = browser.request(url)
+  result = response.body
+
+# url contents to file
+
 proc retrieve(url, filename: string): int =
   var browser = newHttpClient()
   var response = browser.request(url)
